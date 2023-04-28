97960f0ef03ffe4695516751d51bd16aa01797d5
DY-JS <delo1604@ukr.net>
2023-04-28 18:32:38 +0200

    add reduser

--- src/features/position.ts
+++ src/features/position.ts
@@ -13,10 +13,28 @@ const moveLeft = (): MoveLeftPosition => ({ type: 'position/MOVE_LEFT' });
 const moveRight = (): MoveRightPosition => ({ type: 'position/MOVE_RIGHT' });
 const moveUp = (): MoveUpPosition => ({ type: 'position/MOVE_UP' });
 const moveDown = (): MoveDownPosition => ({ type: 'position/MOVE_DOWN' });
-const startPosition = { x: 0, y: 0 };
 
-const positionReducer = (position = startPosition, action: Action) => {
-  return position;
+type Position = { x: number; y: number }; //типизируем state и нач. сост-е
+
+const startPosition: Position = { x: 0, y: 0 };
+
+const positionReducer = (
+  position: Position = startPosition,
+  action: Action
+): Position => {
+  switch (action.type) {
+    case 'position/MOVE_LEFT':
+      return { ...position, x: position.x - 1 };
+    case 'position/MOVE_RIGHT':
+      return { ...position, x: position.x + 1 };
+    case 'position/MOVE_UP':
+      return { ...position, y: position.y - 1 };
+    case 'position/MOVE_DOWN':
+      return { ...position, y: position.y + 1 };
+    default:
+      return position;
+  }
 };
 
+export const actions = { moveLeft, moveRight, moveUp, moveDown };
 export default positionReducer;
