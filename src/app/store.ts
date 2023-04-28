898b1e8228a168aae189d0463fa347f83163f84b
DY-JS <delo1604@ukr.net>
2023-04-28 20:23:36 +0200

    add redux-dev tools

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,6 @@
 // npm i redux @types/redux
 import { combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'; //чтобы пользоваться devTools в хроме
 import amountReducer from '../features/amount';
 import goodsReducer from '../features/goods';
 import positionReducer from '../features/position';
@@ -9,7 +10,7 @@ const reducer = combineReducers({
   goods: goodsReducer,
   position: positionReducer,
 });
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools()); //composeWithDevTools() - чтобы пользоваться devTools в хроме
 
 export type RootState = ReturnType<typeof store.getState>;
 
